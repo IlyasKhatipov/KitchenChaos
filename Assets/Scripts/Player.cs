bd988556222ff5e419142654669b3decf7c14945
Fl1p3r228 <ilyaskhat@mail.ru>
2026-01-25 15:47:22 +0500

    First commit, setup player controller, some post processing, player animations

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] private float moveSpeed = 7f;
+    [SerializeField] private float rotationSpeed = 7f;
+    [SerializeField] private GameInput gameInput;
+
+    private bool isWalking;
+
+    private void Update()
+    {
+        Vector2 inputVector = gameInput.GetMovementVectorNormalized();
+        Vector3 moveDir = new Vector3(inputVector.x, 0, inputVector.y);
+        float moveDistance = moveSpeed * Time.deltaTime;
+        float playerRadius = 0.7f;
+        float playerHeight = 0.7f;
+        bool canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight, playerRadius, moveDir, moveDistance);
+
+        if (!canMove) 
+        {
+            Vector3 moveDirX = new Vector3(moveDir.x, 0, 0).normalized;
+            canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight, playerRadius, moveDirX, moveDistance);
+            if (canMove)
+            {
+                moveDir = moveDirX;
+            }
+            else 
+            {
+                Vector3 moveDirZ = new Vector3(0, 0, moveDir.z).normalized;
+                canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight, playerRadius, moveDirZ, moveDistance);
+                if (canMove)
+                {
+                    moveDir = moveDirZ;
+                }
+                else 
+                {
+
+                }
+            }
+        }
+
+        if (canMove) 
+        {
+            transform.position += moveDir * moveDistance;
+        }
+        
+        isWalking = moveDir != Vector3.zero;
+        if (moveDir != Vector3.zero) 
+        {
+            transform.forward = Vector3.Slerp(transform.forward, moveDir, Time.deltaTime * rotationSpeed);
+        }
+    }
+
+    public bool IsWalking() 
+    {
+        return isWalking;
+    }
+}
