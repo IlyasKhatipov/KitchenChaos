a37e805c547309e50e1f8250542c46c3fb1c1f10
Fl1p3r228 <ilyaskhat@mail.ru>
2026-02-14 03:40:41 +0500

    added delivery manager, including delivery counter and some ui

---
+++ Assets/Scripts/DeliveryManager.cs
@@ -0,0 +1,81 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class DeliveryManager : MonoBehaviour
+{
+
+    public event EventHandler OnRecipeSpawned;
+    public event EventHandler OnRecipeCompleted;
+
+    public static DeliveryManager Instance { get; private set; }
+    [SerializeField] private RecipeListSO recipeListSO;
+
+    private List<RecipeSO> waitingRecipeSOList;
+    private float recipeSpawnTimer;
+    private float recipeSpawnTimerMax = 4f;
+    private int waitingRecipesMax = 4;
+
+    private void Awake()
+    {
+        Instance = this;
+
+        waitingRecipeSOList = new List<RecipeSO>();
+    }
+
+    private void Update()
+    {
+        recipeSpawnTimer -= Time.deltaTime;
+        if (recipeSpawnTimer <= 0f) 
+        {
+            recipeSpawnTimer = recipeSpawnTimerMax;
+            if (waitingRecipeSOList.Count < waitingRecipesMax) 
+            {
+                RecipeSO recipeSO = recipeListSO.recipeSOList[UnityEngine.Random.Range(0, recipeListSO.recipeSOList.Count)];
+                waitingRecipeSOList.Add(recipeSO);
+                OnRecipeSpawned?.Invoke(this, EventArgs.Empty);
+            }
+        }
+    }
+
+    public void DeliverRecipe(PlateKitchenObject plateKitchenObject) 
+    {
+        for (int i = 0; i < waitingRecipeSOList.Count; i++) 
+        {
+            RecipeSO waitingRecipeSO = waitingRecipeSOList[i];
+            if (waitingRecipeSO.kitchenObjectSOList.Count == plateKitchenObject.GetKitchenObjectSOList().Count) 
+            {
+                bool plateCOntentsMatchesRecipe = true;
+                foreach (KitchenObjectSO recipeKitchenObjectSO in waitingRecipeSO.kitchenObjectSOList) 
+                {
+                    bool ingredientFound = false;
+                    foreach (KitchenObjectSO plateKitchenObjectSO in plateKitchenObject.GetKitchenObjectSOList()) 
+                    {
+                        if (plateKitchenObjectSO == recipeKitchenObjectSO) 
+                        {
+                            ingredientFound = true;
+                            break;
+                        }
+                    }
+                    if (!ingredientFound) 
+                    {
+                        plateCOntentsMatchesRecipe = false;
+                    }
+                }
+                if (plateCOntentsMatchesRecipe) 
+                {
+                    waitingRecipeSOList.RemoveAt(i);
+                    OnRecipeCompleted?.Invoke(this, EventArgs.Empty);
+                    return;
+                }
+            }
+        }
+        Debug.Log("player didnt delivered correct recipe");
+    }
+
+    public List<RecipeSO> GetWaitingRecipeSOList() 
+    {
+        return waitingRecipeSOList;
+    }
+}
