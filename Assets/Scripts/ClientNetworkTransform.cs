1801c9cab97a90575220b52229a98dfe8c59985a
Ilyas Khatipov <ilyaskhat@mail.ru>
2026-03-19 17:24:38 +0500

    synced players movement and animations

---
+++ Assets/Scripts/ClientNetworkTransform.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+using Unity.Netcode.Components;
+
+namespace Unity.Multiplayer.Samples.Utilities.ClientAuthority 
+{
+    public class ClientNetworkTransform : NetworkTransform
+    {
+        protected override bool OnIsServerAuthoritative()
+        {
+            return false;
+        }
+    }
+}
